330863e2ee6e50add3b1b17ce1dd0e26776dddac
Ibrahima SOURABIE <ge65yub@mytum.de>
2023-11-12 01:26:43 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict';
+let posts = [
+  {
+    author: 'Tagesschau',
+    image: '',
+    location: 'Hamburg',
+    description: '',
+    number_likes: 1000,
+    add_date: '30 October',
+  },
+];
